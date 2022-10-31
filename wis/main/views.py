87c53a3763dfbef89f096042371583be9ad00a8c
Tricked111 <Shift3114@gmail.com>
2022-10-31 14:48:59 +0100

    add sigIn and update SignUp

--- wis/main/views.py
+++ wis/main/views.py
@@ -58,52 +58,27 @@ def register_user(request):
             form.save()
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
-            #email = form.cleaned_data['email']
-            #firstname = form.cleaned_data['firstname']
-
+            
+            
             user = authenticate(username=username,password=password)
             login(request,user)    
+            
+            """ user_instance = User.objects.filter(username=username).first()
+            Person.objects.create(user=user_instance, firstname=firstname, surname=surname, address=address,
+                                  telephone=telephone, role='s') """
+
+            
             messages.success(request,"Registration Successful!")
             return redirect('/')
+        
+            
+        else:
+            messages.success(request,f"Registration Failed! {form.errors.as_text()}")
+            return redirect('/register')
     
     else:
         form = SignUpForm()
-        
+        print(dir(form))
     return render(request,'register.html',{
         'form' : form,
     })
-
-
-""" 
-
-
-def register_userr(request):
-    if request.method == 'POST':
-
-        form = SignUpForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password2')
-
-            firstname = form.cleaned_data.get('firstname')
-            surname = form.cleaned_data.get('surname')
-            address = form.cleaned_data.get('address')
-            email = form.cleaned_data.get('email')
-            telephone = form.cleaned_data.get('telephone')
-
-            user = authenticate(username=username, password=raw_password)
-
-            login(request, user)
-            messages.success(request,"Registration Successful!")
-            
-            user_instance = User.objects.filter(username=username).first()
-            Person.objects.create(user=user_instance, firstname=firstname, surname=surname, address=address,
-                                  telephone=telephone, role='s')
-
-            return redirect('/')
-    else:
-        messages.success(request,"Registration Failed!")
-        form = SignUpForm()
-    return render(request, 'register.html', {'form': form}) """
